57ef4e67cf7a9e62e0895396852f58a5684dcd13
Jess Chadwick <jesschadwick@gmail.com>
2012-05-18 13:11:09 -0400

    Upgrading to AvalonDock 2.0 beta

---
+++ RazorPad.UI.Application/Views/PanesTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using RazorPad.ViewModels;
+
+namespace RazorPad.Views
+{
+    class PanesTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DocumentEditorTemplate
+        {
+            get;
+            set;
+        }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is RazorTemplateEditorViewModel)
+                return DocumentEditorTemplate;
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
